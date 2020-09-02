5ed1af587522a0ac8327ae5dde571d7228eda0b8
Sanny Do <sanny.do94@gmail.com>
2020-09-02 06:53:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,25 +87,23 @@ def changing_alan
    #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
    #return the entire updated hash
 
-	programmer_hash = 
- 	 	{
-       :grace_hopper => {
-         :known_for => "COBOL",
-         :languages => ["COBOL", "FORTRAN"]
-       },
-       :alan_kay => {
-         :known_for => "GUI",
-         :languages => ["Smalltalk", "LISP"]
-       },
-       :dennis_ritchie => {
-         :known_for => "Unix",
-         :languages => ["C"]
-       }
-     }
+programmer_hash = 
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
 
 end
-hash = { person: { name: 'alan_kay', known_for: 'GUI' } }
-return programmer_hash
 
 def adding_to_dennis
    # add "Assembly" to Dennis Ritchie's languages array

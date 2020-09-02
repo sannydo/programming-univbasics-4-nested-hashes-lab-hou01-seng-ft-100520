58794124699cf4b7400976d333bd17c35d095fb5
Sanny Do <sanny.do94@gmail.com>
2020-09-02 07:23:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,8 @@ programmer_hash =
         :languages => ["C"]
       }
     }
-    programmer_hash = [:alan_kay, :known_for => "GUI"]
+    alans_new_info = "GUI"
+    programmer_hash[:alan_kay][:known_for]
     programmer_hash
 end
 

85755ba2b74cab34fbb9590f854a1645aeb6816d
Sanny Do <sanny.do94@gmail.com>
2020-09-02 06:52:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -42,13 +42,22 @@ end
 	
 
 def dennis_ritchies_language
- 	programmer_hash = 
+	programmer_hash = 
  		{
-       :dennis_ritchie => {
-         :known_for => "Unix",
-         :languages => ["C"]
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
       }
-     }
+    }
+
 end
 
 def adding_matz

4412b07529faa0178a86ae8aa32d3335b5327247
Sanny Do <sanny.do94@gmail.com>
2020-09-02 06:51:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,14 +22,24 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	programmer_hash =
-	 {
-	   :alan_kay => {
-	     :known_for => "Object Orientation",
-	     :languages => ["Smalltalk", "LISP"]
-	   }
-	 }
+  programmer_hash = 
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+
 end
+	
 
 def dennis_ritchies_language
  	programmer_hash = 
